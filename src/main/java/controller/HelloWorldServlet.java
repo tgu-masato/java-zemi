49ace3a55e0cf7a19e2b6a7635b0ca959085d2f7
Kawaguchisan <a141407s@st.u-gakugei.ac.jp>
2019-05-28 01:19:49 +0900

    ファイル名の変更

--- src/main/java/controller/HelloWorldServlet.java
+++ src/main/java/controller/HelloWorldServlet.java
@@ -1,4 +1,4 @@
-package com.github.mhilker.example;
+package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,24 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * This servlet responses with "Hello World!" to GET requests.
- */
 public class HelloWorldServlet extends HttpServlet {
-	
-	/**
-	 * Write "Hello World!" to the response.
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
 		
 		writer.println("Hello World!!");
 		writer.close();			
 	}
-	
 }

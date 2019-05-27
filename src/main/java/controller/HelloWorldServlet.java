7f546290bbd7cf3e72c1ee4b5fcd4471491ec85d
Kawaguchisan <a141407s@st.u-gakugei.ac.jp>
2019-05-28 01:50:47 +0900

    web.xml の削除

--- src/main/java/controller/HelloWorldServlet.java
+++ src/main/java/controller/HelloWorldServlet.java
@@ -3,10 +3,12 @@ package controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
